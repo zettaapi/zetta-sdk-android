2a365f19a17a76dd5414e8a774f0247a4b0aad7d
Paul Blundell <paul@amp.build>
2016-06-04 17:25:06 +0100

    updates test to failfast

--- zettakit/src/androidTest/java/com/apigee/zettakit/ApplicationTest.java
+++ zettakit/src/androidTest/java/com/apigee/zettakit/ApplicationTest.java
@@ -11,6 +11,7 @@ import com.apigee.zettakit.callbacks.ZIKServersCallback;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -19,17 +20,18 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
     public void testGET() throws InterruptedException {
         final CountDownLatch signal = new CountDownLatch(1);
         ZIKSession.init(this.getContext());
         final ZIKSession sharedSession = ZIKSession.getSharedSession();
-        sharedSession.getRoot("http://v1-staging.iot.apigee.net/", new ZIKRootCallback() {
+        sharedSession.getRoot("http://stage.zettaapi.org", new ZIKRootCallback() {
             @Override
             public void onSuccess(@NonNull ZIKRoot root) {
                 sharedSession.getServers(root, new ZIKServersCallback() {
                     @Override
                     public void onFinished(@Nullable List<ZIKServer> servers) {
-                        if( servers != null && !servers.isEmpty() ) {
+                        if (servers != null && !servers.isEmpty()) {
                             ZIKServer server = servers.get(0);
                             sharedSession.getDevices(server, new ZIKDevicesCallback() {
                                 @Override
@@ -37,15 +39,18 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
                                     signal.countDown();
                                 }
                             });
+                        } else {
+                            throw new IllegalStateException("unexpected code path for this test");
                         }
                     }
                 });
             }
+
             @Override
             public void onError(@NonNull String error) {
-                signal.countDown();
+                throw new IllegalStateException("unexpected code path for this test");
             }
         });
-        signal.await();
+        signal.await(30, TimeUnit.SECONDS);
     }
 }
